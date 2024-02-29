74229af6238e02bb736fab6489a6f400eb1fe83d
dmitryKosyan <148568152+dmitryKosyan@users.noreply.github.com>
2024-02-29 15:53:27 +0300

    services

---
+++ docs/js/projects.bundle.js
@@ -0,0 +1,1 @@
+!function(){const o=gsap.timeline();o.fromTo(".portfolio__title",{opacity:0},{opacity:1,duration:3},.4),o.fromTo(".portfolio__text",{y:100,opacity:0},{opacity:1,y:0,duration:1},.4)}();
